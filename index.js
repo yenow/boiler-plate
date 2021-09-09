03fd393dd57697f7ed7766d76f5f7411325a7c21
sinyoung yun <54181528+yenow@users.noreply.github.com>
2021-09-09 22:25:27 +0900

    login developing

--- index.js
+++ index.js
@@ -14,26 +14,65 @@ app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
+
+// useCreateIndex : true
+// useFindAndModify : false
 mongoose.connect(config.mongoURI,{
     useNewUrlParser : true,
-    useUnifiedTopology : true,
-    // useCreateIndex : true,
-    // useFindAndModify : false
-}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+    useUnifiedTopology : true
+},function (err) {
+    if (err) {
+        console.log(err)
+        return false;
+    }
+    console.log('MongoDB Connected...')
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! hihi')
 })
 
 app.post('/register',(req,res) => {
     // 회원 가입시 필요한 정보를 cilent에서 가져오면
     // 그것들을 데이터베이스에 넣어준다.
     const user = new User(req.body);
-    user.save((err,userInfo)=>{
-        if(err) return res.json({success : false,err})
+
+    user.save((err) => {
+        if(err) {
+            return res.json({success : false,err})
+        }
         return res.status(200).json({
             success : true
         })
+    })
+})
+
+app.post('/login', (req,res) => {
+
+    /* 1. 요청된 이메일을 DB에 있는지 찾는다 */
+    User.findOne({ email :  req.body.email }, (err,userInfo) => {
+        if (!userInfo) {
+            return res.json({
+                loginSuccess : false,
+                message : "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        /* 2. EMAIL이 있다면 비밀번호가 같은지 확인 */
+        userInfo.comparePassword(req.body, (err,isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess : false,
+                    message : "비밀번호가 틀렸습니다."
+                })
+            }
+
+            /* 3. 비밀번호가 맞다면 토큰 생성 */
+            userInfo.generateToken((err,user) => {
+
+            })
+        })
+
     })
 })
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// const myPlaintextPassword = 's0/\/\P4$$w0rD';
+// const someOtherPlaintextPassword = 'not_bacon';
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name : {
         type : String,
         maxlength : 50
@@ -26,6 +30,39 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 })
+
+userSchema.pre('save',function (next) {
+
+    let user = this;
+
+    if (user.isModified('password')) {  // 비밀번호가 바뀌었을때
+
+        // 솔트 생성, saltRounds가 필요
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)    // save함수로 보내줌(index.js의 user.save())
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)    // save함수로 보내줌(index.js의 user.save())
+
+                // Store hash in your password DB.
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+        next()
+    }
+}) // save() 함수를 실행하기전에 콜백함수를 먼저 실행함
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    // plainPassword
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err)
+        cb(null,isMatch)
+    })
+
+}
 
 const User = mongoose.model('User', userSchema)
 
